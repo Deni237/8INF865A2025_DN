d23675a5dc60ff577a5f7abf9fe2d118d883fee3
Denilson <nzognengdenilson@yahoo.com>
2025-10-15 14:33:16 -0400

    Etape7 - creation des ressources de chaine

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,8 +37,9 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingImage(
-                        "Happy Birthday Joseph",
-                        from="From Denilson")
+                        stringResource(R.string.happy_birthday_text),
+                        from= stringResource(R.string.signature_text)
+                    )
                 }
             }
         }
@@ -94,7 +96,8 @@ fun GreetingImage(message:String,from:String, modifier:Modifier = Modifier){
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
         GreetingImage(
-            "Happy Birthday Joseph",
-            from="From Denilson")
+            stringResource(R.string.happy_birthday_text),
+            from= stringResource(R.string.signature_text)
+        )
     }
 }
