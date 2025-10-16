eaada1e56b5030dc9556a41960c2a6daf2d1db8d
Denilson <nzognengdenilson@yahoo.com>
2025-10-16 19:54:17 -0400

    Etape4 - ajout d'un champ de texte pour le pourcentage de pourboire

--- TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -19,6 +19,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -68,9 +69,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipTimeLayout() {
     var amountInput by remember { mutableStateOf("") }
+    var tipInput by remember { mutableStateOf("") }
 
     val amount = amountInput.toDoubleOrNull()?:0.0
-    val tip = calculateTip(amount)
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+
+    val tip = calculateTip(amount, tipPercent)
+
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -85,7 +90,18 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-        EditNumberField(amountInput, onValueChange = {amountInput = it}, modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+        EditNumberField(
+            R.string.bill_amount,
+            amountInput,
+            onValueChange = {amountInput = it},
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+
+        EditNumberField(
+            R.string.how_was_the_service,
+            tipInput,
+            {tipInput = it},
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+
         Text(
             text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
@@ -95,14 +111,14 @@ fun TipTimeLayout() {
 }
 
 @Composable
-fun EditNumberField(value:String, onValueChange:(String)->Unit, modifier:Modifier = Modifier){
+fun EditNumberField(@StringRes label:Int, value:String, onValueChange:(String)->Unit, modifier:Modifier = Modifier){
 
 
     TextField(
         value = value,
         onValueChange = onValueChange,
         singleLine = true,
-        label = { Text(stringResource(R.string.bill_amount)) },
+        label = { Text(stringResource(label)) },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         modifier = modifier
     )
